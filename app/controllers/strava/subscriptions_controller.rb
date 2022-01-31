86ab232796c59618ce0a5109bf94a19e523a57ae
Maksim Rukomoynikov <maksim@rukomoynikov.ru>
2022-01-31 23:28:29 +0300

    Debugging

--- app/controllers/strava/subscriptions_controller.rb
+++ app/controllers/strava/subscriptions_controller.rb
@@ -1,9 +1,13 @@
 module Strava
   class SubscriptionsController < ApplicationController
     def confirm
+      p params["hub.challenge"]
+
       render json: {
         "hub.challenge": params["hub.challenge"]
       }
     end
   end
 end
+
+# "hub.challenge": params["hub.challenge"]
